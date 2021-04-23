583c70907a1dbf16a310320f29f968dcd46dade4
Enmanuel Alvarez Ortiz <>
2021-04-23 03:23:53 +0000

    Reto 3

---
+++ py1_rima/py2_retos1/reto3.py
@@ -0,0 +1,15 @@
+x=15
+y=3
+print(x,'+',y,'=',x+y)
+print(x,'-',y,'=',x-y)
+print(x,'*',y,'=',x*y)
+print(x,'/',y,'=',x/y)
+x,y=2,3
+print(x,'elevado a',y,'=',x**y)
+x=11
+print(x,'división entera entre',y,'=',x//y)
+print(x,'módulo',y,'=',x%y)
+#operadores lógicos
+print(x,'es mayor que',y,x>y)
+print(x,'es menor que',y,x<y)
+print(y,'<',x,'<20',y<x<20)
