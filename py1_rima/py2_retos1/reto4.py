8c9ff47cc9464c42891bfcc50f9f42f8661e6faa
Enmanuel Alvarez Ortiz <>
2021-04-23 03:27:22 +0000

    Reto 4

---
+++ py1_rima/py2_retos1/reto4.py
@@ -0,0 +1,8 @@
+x = int(input("¿Cuál es el primer número?")) #Preguntamos al usuario qué número quiere
+y = int(input("¿Cuál es el segundo número?"))
+if x>y:
+  print('El primer número es mayor.')
+elif x<y:
+  print('El primer número es menor.')
+else:
+  print('Es el mismo número.')
